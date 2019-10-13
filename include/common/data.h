781b808a2b1228e270193dd3a7a94b10bb4b38fe
Patrick Fitzgerald <fitzgerald.patrick@gmail.com>
2019-10-13 12:11:07 -0600

    Updating data.h, make safePoint

--- include/common/data.h
+++ include/common/data.h
@@ -28,6 +28,9 @@
  *   @return = length of ASCII string
  *
  **/
+/*
+ * function definition from data.c
+ */
 uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base);
 
 /*********** my_atoi() **************/
@@ -42,6 +45,9 @@ uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base);
  *   @return = 32-bit signed integer
  *
  **/
+ /** function definition from data.c
+  *
+  **/
 int32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base);
 
 #endif
